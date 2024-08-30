997451b795169e90220a8a164aeff7931d7223a4
niekwit <41302859+niekwit@users.noreply.github.com>
2024-08-30 08:36:57 +0100

    working for viral data now

--- workflow/scripts/deseq2.R
+++ workflow/scripts/deseq2.R
@@ -1,249 +1,199 @@
-# redirect R output to log
-log <- file(snakemake@log[[1]], open="wt")
+# Redirect R output to log
+log <- file(snakemake@log[[1]], open = "wt")
 sink(log, type = "output")
 sink(log, type = "message")
 
+# Load packages
 library(DESeq2)
+library(GenomicFeatures)
+library(tximport)
 library(dplyr)
 library(stringr)
-library(biomaRt)
-library(openxlsx)
+library(rtracklayer)
+library(readr)
 
-# load Snakemake variables
+# Load Snakemake variables
 count.files <- snakemake@input[["counts"]]
 genome <- snakemake@params[["genome"]]
+gtf <- snakemake@input[["gtf"]]
+strand <- snakemake@params[["strand"]]
 
-# Use first count file as template for count matrix
-countMatrix <- read.delim(count.files[1]) 
-names(countMatrix) <- c("index", sub("ReadsPerGene.out.tab","",basename(count.files[1])))
-
-# add all count data to countMatrix
-for (i in seq(from=2,to=length(count.files))) {
-    sample <- sub(".cntTable","",basename(count.files[i]))
-    df <- read.delim(count.files[i])
-    colnames(df) <- c("index", sample)
-    
-    countMatrix <- full_join(countMatrix, 
-                             df, 
-                             by="index")
-  }
-
-# remove lines with all 0s
-countMatrix <- countMatrix[rowSums(countMatrix[,2:ncol(countMatrix)]) > 0,]
-
-# create named index
-rownames(countMatrix) <- countMatrix$index
-countMatrix$index <- NULL
-
-#### Load experiment information ####
+# Load experiment information
 samples <- read.csv("config/samples.csv", header=TRUE)
 genotypes <- unique(samples$genotype)
 treatments <- unique(samples$treatment)
+design <- snakemake@params[["design"]]
 
-if (length(treatments) > 1){
-  samples$comb <- paste0(samples$genotype,"_",samples$treatment)
+if (length(genotypes) > 1 & length(treatments) > 1) {
+  samples$comb <- paste0(samples$genotype, "_", samples$treatment)
+} else if (length(genotypes) > 1 & length(treatments) == 1) {
+  samples$comb <- samples$genotype
+} else if (length(genotypes) == 1 & length(treatments) > 1) {
+  samples$comb <- samples$treatment
 } else {
-  samples$comb <- paste0(samples$genotype)
+  stop("Error: Not enough genotypes or treatments for differential expression analysis")
 }
 
-# check if batch column exists
-if ("batch" %in% colnames(samples)){
+# Check if batch column exists
+if ("batch" %in% colnames(samples)) {
   batches <- unique(samples$batch)
+  samples$batch <- as.factor(samples$batch)
 } else {
+  # Add batch column with just one batch
+  samples$batch <- as.factor("1")
   batches <- 1
 }
 
-# check if any samples have been omitted from samples.csv and remove from count.files
-# this allows for a re-run of DESeq2 without having to re-run the entire pipeline
-all_samples <- samples$sample
+# Create txdb from GTF
+txdb <- makeTxDbFromGFF(gtf)
 
-if (length((all_samples)) != length(count.files)){
-  omitted <- count.files[!(basename(gsub(pattern = "\\ReadsPerGene.out.tab", "", count.files)) %in% all_samples)]
-  print("WARNING: Some samples have been omitted from samples.csv. Resuming without these samples:")
-  for (i in seq(omitted)){
-    print(basename(gsub(pattern = "\\.ReadsPerGene.out.tab", "", omitted[i])))
-    
+# Create transcript to gene file
+k <- keys(txdb, keytype = "TXNAME")
+tx2gene <- AnnotationDbi::select(txdb, k, "GENEID", "TXNAME")
+
+# Gene annotation info
+db <- rtracklayer::import(gtf)
+gene.info <- data.frame(ensembl_gene_id = db$gene_id, 
+                        external_gene_name = db$gene_name) %>%
+  filter(!duplicated(ensembl_gene_id))
+
+# Create count matrix
+countMatrix <- read.delim(count.files[1], 
+                          header = FALSE,
+                          skip = 4) %>%
+  dplyr::select(V1)
+names(countMatrix) <- "index"
+
+for (i in seq_along(count.files)) {
+  sample <- sub(".ReadsPerGene.out.tab", "", basename(count.files[i]))
+  df <- read.delim(count.files[i],
+                   header = FALSE,
+                   skip = 4)
+  if (strand == "unstranded") {
+    df <- df %>% dplyr::select(V1, V2)
+  } else if (strand == "yes") {
+    df <- df %>% dplyr::select(V1, V3)
+  } else if (strand == "reverse") {
+    df <- df %>% dplyr::select(V1, V4)
   }
-  count.files <- count.files[basename(gsub(pattern = "\\ReadsPerGene.out.tab$", "", count.files)) %in% all_samples]
+  colnames(df) <- c("index", sample)
+  
+  countMatrix <- full_join(countMatrix,
+                           df,
+                           by = "index")
 }
 
-if (batch == 1){
+# Remove lines with all 0s
+countMatrix <- countMatrix[rowSums(countMatrix[,2:ncol(countMatrix)]) > 0,]
+
+# Create named index
+rownames(countMatrix) <- countMatrix$index
+countMatrix$index <- NULL
+
+# Create DESeqDataSet
+if (length(batches) == 1){
+  print("Not including batch factor in DESeq2 design...")
   dds <- DESeqDataSetFromMatrix(countData = countMatrix,
-                              colData = samples,
-                              design = ~comb)
+                                colData = samples,
+                                design = ~comb)
 } else {
+  print("Including batch factor in DESeq2 design...")
   dds <- DESeqDataSetFromMatrix(countData = countMatrix,
-                              colData = samples,
-                              design = ~batch + comb)
+                                colData = samples,
+                                design = ~batch + comb)
 }
 
-# save DESeqDataSet to file (input for other scripts)
-save(dds, file=snakemake@output[["rdata"]])
+# Save dds to file (input for other scripts)
+save(dds, file = snakemake@output[["rdata"]])
 
-# load reference samples
-references <- unique(samples[samples$reference == "yes" ,]$comb)
-if (length(references) == 0){
-  stop("ERROR: No reference samples found. Please check your samples.csv file.")
-}
+# Load reference samples
+references <- unique(samples[samples$reference == "yes", ]$comb)
 
-# create nested lists to store all pairwise comparisons (top level:references, lower level: samples without reference)
-df.list.genes <- vector(mode="list", length=length(references))
-for (i in seq_along(references)){
-  df.list.genes[[i]] <- vector(mode="list", length=(length(unique(samples$comb)) - 1 ))
-}
+# List to store data from each contrast
+resList <- list()
 
-df.list.te <- vector(mode="list", length=length(references))
-for (i in seq_along(references)){
-  df.list.te[[i]] <- vector(mode="list", length=(length(unique(samples$comb)) - 1 ))
-}
+# For each reference sample, perform pairwise comparisons with all the other samples
+for (r in seq_along(references)){
+  cat(paste0("Setting reference level: ",references[r], " (",r,"/",length(references),")\n"))
 
-tryCatch({
-  # Annotate non-TE Ensembl gene IDs with biomaRt
-  genes <- row.names(countMatrix)
-  if (grepl("hg",genome, fixed=TRUE)) {
-    ensembl <- useEnsembl(biomart="genes", 
-                          dataset = "hsapiens_gene_ensembl", 
-                          mirror = "www")
-    genes <- genes[grepl("ENSG[0-9]{11}+", genes, perl=TRUE)]
-    
-  } else if (grepl("mm",genome, fixed=TRUE)){
-    ensembl <- useEnsembl(biomart="genes", 
-                          dataset = "mmusculus_gene_ensembl", 
-                          mirror = "www")
-    genes <- genes[grepl("ENSMUSG[0-9]{11}+", genes, perl=TRUE)]
-  } # add other genomes later
-  
-  # gene annotation
-  gene.info <- getBM(filters = "ensembl_gene_id", 
-                     attributes = c("ensembl_gene_id", 
-                                    "external_gene_name",
-                                    "description",
-                                    "gene_biotype", 
-                                    "chromosome_name",
-                                    "start_position",
-                                    "end_position", 
-                                    "percentage_gene_gc_content"), 
-                     values = genes,
-                     mart = ensembl)
-}, error = function(error) {
-  # biomaRt is very prone to errors, so catch errors here and print a more informative error message
-  message("bioMart error. There might be an issue with the servers. Please run Snakemake again later.")
-  message("Here's the original error message:")
-  message(conditionMessage(error))
-  }
-)
-
-# performs pair-wise comparisons for each reference sample
-for (r in seq(references)){
-  # copy dds
+  # Copy dds
   dds_relevel <- dds
-  
-  # relevel dds to reference sample
+
+  # For each reference sample, set it as reference in dds
   dds_relevel$comb <- relevel(dds$comb, ref = references[r])
-  
-  # differential expression analysis
+
+  # Differential expression analysis
   dds_relevel <- DESeq(dds_relevel)
-  
-  # get all pairwise comparisons
+
+  # Get comparisons
   comparisons <- resultsNames(dds_relevel)
-  comparisons <- strsplit(comparisons," ")
+  comparisons <- strsplit(comparisons, " ")
   comparisons[1] <- NULL
+
   
-  # create df for each comparison
-  for (c in seq(comparisons)){
-    
-    # get name of comparison
+  # Create df for each comparison
+  for (c in seq_along(comparisons)){
     comparison <- comparisons[[c]]
-    comparison <- str_replace(comparison,"comb_","") 
-    
-    res <- results(dds_relevel, name=comparisons[[c]])
-    
+    comparison <- str_replace(comparison, "comb_", "") # Get name
+    print(paste0("Specifiying contrast: ",comparison, " (",c,"/",length(comparisons),")"))
+
+    res <- results(dds_relevel, name = comparisons[[c]])
     df <- as.data.frame(res) %>%
-      mutate(ensembl_gene_id = res@rownames, .before=1)
-    
-    # get non-TE genes
-    if (grepl("hg",genome) ==TRUE) {
-      df.genes <- df[grepl("ENSG[0-9]{11}+",df$ensembl_gene_id, perl=TRUE),] 
-    } else if (grepl("mm",genome) == TRUE){
-      df.genes <- df[grepl("ENSMUSG[0-9]{11}+",df$ensembl_gene_id, perl=TRUE),] 
-    } # add other genomes later
-    
-    # get TE genes
-    if (grepl("hg",genome) ==TRUE) {
-      df.te <- df[!grepl("ENSG[0-9]{11}+",df$ensembl_gene_id, perl=TRUE),] 
-    } else if (grepl("mm",genome) == TRUE){
-      df.te <- df[!grepl("ENSMUSG[0-9]{11}+",df$ensembl_gene_id, perl=TRUE),] 
-    } # add other genomes later
-    
-    # add gene annotation to df.genes
-    df.genes <- left_join(df.genes,gene.info,by="ensembl_gene_id")
-    
-    # get normalised read counts for each sample  
-    temp <- as.data.frame(counts(dds_relevel, normalized=TRUE))
+      mutate(ensembl_gene_id = res@rownames, .before = 1)
+
+    # Annotate df
+    #df$ensembl_gene_id <- gsub("\\.[0-9]*","",df$ensembl_gene_id) # Tidy up gene IDs
+    df <- left_join(df,gene.info,by = "ensembl_gene_id") %>%
+      relocate(external_gene_name, .after = ensembl_gene_id)
+
+    # Remove genes with baseMean zero
+    df <- df[df$baseMean != 0, ]
+
+    # Add normalised read counts for each sample to df
+    temp <- as.data.frame(counts(dds_relevel, normalized = TRUE))
     temp$ensembl_gene_id <- row.names(temp)
-    temp$ensembl_gene_id <- gsub("\\.[0-9]*","",temp$ensembl_gene_id) #tidy up gene IDs
+    #temp$ensembl_gene_id <- gsub("\\.[0-9]*", "", temp$ensembl_gene_id) # Tidy up gene IDs
     names(temp)[1:length(dds_relevel@colData@listData$sample)] <- dds_relevel@colData@listData$sample
-    
-    # add normalised read counts to df.genes
-    df.genes <- left_join(df.genes,temp,by="ensembl_gene_id")
-    df.genes <- df.genes %>%
-      mutate(contrast_name = comparison, .before=1)
-    
-    # add df.genes to df.list.genes
-    df.list.genes[[r]][[c]] <- df.genes
-    
-    # add normalised read counts to df.te
-    df.te <- left_join(df.te,temp,by="ensembl_gene_id")
-    df.te <- df.te %>%
-      mutate(contrast_name = comparison, .before=1)
-    
-    # add df.te to df.list.te
-    df.list.te[[r]][[c]] <- df.te
-    
+
+    df <- left_join(df, temp, by = "ensembl_gene_id")
+
+    # Order data for padj
+    df <- df[order(df$padj), ]
+
+    # Sheet title can be max 31 characters
+    # Change column with contrast name and change it to a number (counter) if too long
+    if (nchar(comparison) > 31) {
+      # change names to number
+      comparison <- c
+    }
+    df <- df %>%
+      mutate(contrast_name = comparison, .before = 1)
+
+    # Create column with whether gene is viral or not
+    df <- df %>% 
+      mutate(class = case_when(
+        grepl("^ENS", ensembl_gene_id, ignore.case = TRUE) ~ "non-viral",
+        TRUE ~ "viral"
+      )) %>%
+      relocate(class, .after = external_gene_name)
+
+    # Save df to resList
+    resList[[(length(resList) + 1)]] <- df
   }
 }
 
-# function to flatten nested lists (https://stackoverflow.com/questions/16300344/how-to-flatten-a-list-of-lists/41882883#41882883)
-flattenlist <- function(x){  
-  morelists <- sapply(x, function(xprime) class(xprime)[1]=="list")
-  out <- c(x[!morelists], unlist(x[morelists], recursive=FALSE))
-  if(sum(morelists)){ 
-    Recall(out)
-  }else{
-    return(out)
-  }
+# Get contrast names from each df in list
+names <- lapply(resList, function(x) unique(x$contrast_name))
+
+# Name data frames in list
+names(resList) <- names
+
+# Write each df to separate csv file
+for (i in seq(resList)){
+  write_csv(resList[[i]],
+            paste0("results/deseq2/", names(resList)[i], ".csv"))
 }
 
-# flatten lists
-df.list.genes <- flattenlist(df.list.genes)
-df.list.te <- flattenlist(df.list.te)
-
-# get contrast names from each df in list
-names.genes <- lapply(df.list.genes, function(x) unique(x$contrast_name))
-names(df.list.genes) <- names.genes
-
-names.te <- lapply(df.list.te, function(x) unique(x$contrast_name))
-names(df.list.te) <- names.te
-
-# write each df also to separate csv file
-save2csv <- function(df.list, type){
-  for (i in seq(df.list)){
-    write.csv(df.list[[i]], 
-              paste0("results/deseq2/", names(df.list)[i], type, ".csv"), 
-              row.names = FALSE)
-  }
-}
-save2csv(df.list.genes, "_genes")
-save2csv(df.list.te, "_te")
-
-# write output to file
-write.xlsx(df.list.genes, 
-           snakemake@output[["genes"]],
-           colNames = TRUE)
-
-write.xlsx(df.list.te,
-           snakemake@output[["te"]],
-           colNames = TRUE)
-
+# Close log file
 sink(log, type = "output")
 sink(log, type = "message")

4140c11764b1300243a66f45f5ef5957341082a6
niekwit <41302859+niekwit@users.noreply.github.com>
2024-09-04 13:11:30 +0100

    Initial commit

---
+++ .test_se/reads/download.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#https://www.science.org/doi/full/10.1126/science.1227919
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR592/SRR592966/SRR592966.fastq.gz -o 5hr_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR592/SRR592965/SRR592965.fastq.gz -o 72hr_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR592/SRR592963/SRR592963.fastq.gz -o 5hr_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR592/SRR592968/SRR592968.fastq.gz -o 72hr_2.fastq.gz5hr
+
+#Only first 1E6 lines are kept for all samples (head -1000000)

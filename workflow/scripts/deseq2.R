fb85101bd5f187ee9f282a527e089f6a64d15dd7
niekwit <41302859+niekwit@users.noreply.github.com>
2026-01-23 11:07:04 +0000

    many improvements

--- workflow/scripts/deseq2.R
+++ workflow/scripts/deseq2.R
@@ -4,27 +4,27 @@ sink(log, type = "output")
 sink(log, type = "message")
 
 # Load packages
+library(tidyverse)
 library(DESeq2)
 library(GenomicFeatures)
 library(tximport)
-library(dplyr)
-library(stringr)
 library(rtracklayer)
-library(readr)
+
 
 # Load Snakemake variables
-count.files <- snakemake@input[["counts"]]
+count_files <- snakemake@input[["counts"]]
 genome <- snakemake@params[["genome"]]
 viralgenome <- snakemake@params[["viralgenome"]]
 gtf <- snakemake@input[["gtf"]]
 strand <- snakemake@params[["strand"]]
+lfc_shrinkage <- as.logical(snakemake@config[["lfc_shrinkage"]])
 
 # Load experiment information
-samples <- read.csv("config/samples.csv", header=TRUE)
+samples <- read.csv("config/samples.csv", header = TRUE)
 genotypes <- unique(samples$genotype)
 treatments <- unique(samples$treatment)
-design <- snakemake@params[["design"]]
 
+# Determine the experimental grouping
 if (length(genotypes) > 1 & length(treatments) > 1) {
   samples$comb <- paste0(samples$genotype, "_", samples$treatment)
 } else if (length(genotypes) > 1 & length(treatments) == 1) {
@@ -32,10 +32,12 @@ if (length(genotypes) > 1 & length(treatments) > 1) {
 } else if (length(genotypes) == 1 & length(treatments) > 1) {
   samples$comb <- samples$treatment
 } else {
-  stop("Error: Not enough genotypes or treatments for differential expression analysis")
+  stop(
+    "Not enough genotypes or treatments for differential expression analysis"
+  )
 }
 
-# Check if batch column exists
+# Handle Batch Factor
 if ("batch" %in% colnames(samples)) {
   batches <- unique(samples$batch)
   samples$batch <- as.factor(samples$batch)
@@ -54,22 +56,20 @@ tx2gene <- AnnotationDbi::select(txdb, k, "GENEID", "TXNAME")
 
 # Gene annotation info
 db <- rtracklayer::import(gtf)
-gene.info <- data.frame(ensembl_gene_id = db$gene_id, 
-                        external_gene_name = db$gene_name) %>%
+gene.info <- data.frame(
+  ensembl_gene_id = db$gene_id,
+  external_gene_name = db$gene_name
+) %>%
   filter(!duplicated(ensembl_gene_id))
 
 # Create count matrix
-countMatrix <- read.delim(count.files[1], 
-                          header = FALSE,
-                          skip = 4) %>%
+countMatrix <- read.delim(count_files[1], header = FALSE, skip = 4) %>%
   dplyr::select(V1)
 names(countMatrix) <- "index"
 
-for (i in seq_along(count.files)) {
-  sample <- sub(".ReadsPerGene.out.tab", "", basename(count.files[i]))
-  df <- read.delim(count.files[i],
-                   header = FALSE,
-                   skip = 4)
+for (i in seq_along(count_files)) {
+  sample <- sub(".ReadsPerGene.out.tab", "", basename(count_files[i]))
+  df <- read.delim(count_files[i], header = FALSE, skip = 4)
   if (strand == "unstranded") {
     df <- df %>% dplyr::select(V1, V2)
   } else if (strand == "yes") {
@@ -78,34 +78,71 @@ for (i in seq_along(count.files)) {
     df <- df %>% dplyr::select(V1, V4)
   }
   colnames(df) <- c("index", sample)
-  
-  countMatrix <- full_join(countMatrix,
-                           df,
-                           by = "index")
+
+  countMatrix <- full_join(countMatrix, df, by = "index")
 }
 
 # Remove lines with all 0s
-countMatrix <- countMatrix[rowSums(countMatrix[,2:ncol(countMatrix)]) > 0,]
+countMatrix <- countMatrix[rowSums(countMatrix[, 2:ncol(countMatrix)]) > 0, ]
 
 # Create named index
 rownames(countMatrix) <- countMatrix$index
 countMatrix$index <- NULL
 
 # Create DESeqDataSet
-if (length(batches) == 1){
-  print("Not including batch factor in DESeq2 design...")
-  dds <- DESeqDataSetFromMatrix(countData = countMatrix,
-                                colData = samples,
-                                design = ~comb)
-} else {
-  print("Including batch factor in DESeq2 design...")
-  dds <- DESeqDataSetFromMatrix(countData = countMatrix,
-                                colData = samples,
-                                design = ~batch + comb)
-}
+design_formula <- if (length(batches) > 1) ~ batch + comb else ~comb
+dds <- DESeqDataSetFromMatrix(
+  countData = countMatrix,
+  colData = samples,
+  design = design_formula
+)
 
-# Save dds to file (input for other scripts)
+# Calculate size factors to normalize for sequencing depth
+dds <- estimateSizeFactors(dds)
+
+# Generate Batch-Corrected Data
+# VST is better for visualization than raw normalized counts
+vsd <- vst(dds, blind = FALSE)
+batch_multipliers <- rep(1, ncol(dds))
+names(batch_multipliers) <- colnames(dds)
+if (length(batches) > 1) {
+  # Create a design matrix for the biological groups of interest.
+  # This ensures limma preserves the 'comb' differences while removing 'batch'.
+  mod <- model.matrix(~comb, data = colData(vsd))
+
+  # Remove batch effect from the transformed values
+  mat <- assay(vsd)
+  mat_corrected <- limma::removeBatchEffect(
+    mat,
+    batch = vsd$batch,
+    design = mod
+  )
+
+  # Calculate an "effective multiplier" for each sample.
+  # We compare the mean intensity after correction vs before correction.
+  # This provides a linear factor that approximates the batch shift.
+  for (i in seq_along(colnames(dds))) {
+    # Ratio of geometric means (effectively)
+    batch_multipliers[i] <- mean(2^mat_corrected[, i]) / mean(2^mat[, i])
+  }
+
+  assay(vsd) <- mat_corrected
+}
+batch_corrected_counts <- as.data.frame(assay(vsd))
+
+# Save dds for downstream use
 save(dds, file = snakemake@output[["rdata"]])
+
+# Export comprehensive Scaling Factors for BigWig scaling
+sf_df <- data.frame(
+  sample = colnames(dds),
+  depth_size_factor = sizeFactors(dds),
+  batch_multiplier = batch_multipliers,
+  # This is the final value you should use for --scaleFactor in deepTools
+  final_bigwig_scalefactor = (1 / sizeFactors(dds)) * batch_multipliers
+)
+write_csv(sf_df, snakemake@output[["scale_factors"]])
+
 
 # Load reference samples
 references <- unique(samples[samples$reference == "yes", ]$comb)
@@ -113,73 +150,83 @@ references <- unique(samples[samples$reference == "yes", ]$comb)
 # List to store data from each contrast
 resList <- list()
 
-# For each reference sample, perform pairwise comparisons with all the other samples
-for (r in seq_along(references)){
-  cat(paste0("Setting reference level: ",references[r], " (",r,"/",length(references),")\n"))
+# For each reference sample, perform pairwise comparisons
+# with all the other samples
+for (r in seq_along(references)) {
+  cat(paste0(
+    "Setting reference level: ",
+    references[r],
+    " (",
+    r,
+    "/",
+    length(references),
+    ")\n"
+  ))
 
-  # Copy dds
   dds_relevel <- dds
-
-  # For each reference sample, set it as reference in dds
   dds_relevel$comb <- relevel(dds$comb, ref = references[r])
-
-  # Differential expression analysis
   dds_relevel <- DESeq(dds_relevel)
 
   # Get comparisons
-  comparisons <- resultsNames(dds_relevel)
-  comparisons <- strsplit(comparisons, " ")
-  comparisons[1] <- NULL
+  comp_names <- resultsNames(dds_relevel)
+  comp_names <- comp_names[
+    comp_names != "Intercept" & !grepl("batch", comp_names)
+  ]
 
-  
   # Create df for each comparison
-  for (c in seq_along(comparisons)){
-    comparison <- comparisons[[c]]
-    comparison <- str_replace(comparison, "comb_", "") # Get name
-    print(paste0("Specifiying contrast: ",comparison, " (",c,"/",length(comparisons),")"))
+  for (comp in comp_names) {
+    clean_name <- str_replace(comp, "comb_", "")
+    res <- results(dds_relevel, name = comp)
 
-    res <- results(dds_relevel, name = comparisons[[c]])
-    df <- as.data.frame(res) %>%
-      mutate(ensembl_gene_id = res@rownames, .before = 1)
-
-    # Annotate df
-    #df$ensembl_gene_id <- gsub("\\.[0-9]*","",df$ensembl_gene_id) # Tidy up gene IDs
-    df <- left_join(df,gene.info,by = "ensembl_gene_id") %>%
-      relocate(external_gene_name, .after = ensembl_gene_id)
-
-    # Remove genes with baseMean zero
-    df <- df[df$baseMean != 0, ]
-
-    # Add normalised read counts for each sample to df
-    temp <- as.data.frame(counts(dds_relevel, normalized = TRUE))
-    temp$ensembl_gene_id <- row.names(temp)
-    #temp$ensembl_gene_id <- gsub("\\.[0-9]*", "", temp$ensembl_gene_id) # Tidy up gene IDs
-    names(temp)[1:length(dds_relevel@colData@listData$sample)] <- dds_relevel@colData@listData$sample
-
-    df <- left_join(df, temp, by = "ensembl_gene_id")
-
-    # Order data for padj
-    df <- df[order(df$padj), ]
-
-    # Sheet title can be max 31 characters
-    # Change column with contrast name and change it to a number (counter) if too long
-    if (nchar(comparison) > 31) {
-      # change names to number
-      comparison <- c
+    if (lfc_shrinkage) {
+      # Shrink effect size for better ranking of genes
+      res <- lfcShrink(
+        dds_relevel,
+        coef = comp,
+        res = res,
+        type = "apeglm"
+      )
     }
-    df <- df %>%
-      mutate(contrast_name = comparison, .before = 1)
 
-    # Create column with whether gene is viral or not
-    df <- df %>% 
-      mutate(class = case_when(
-        grepl("^ENS", ensembl_gene_id, ignore.case = TRUE) ~ genome,
-        TRUE ~ viralgenome
-      )) %>%
+    # Get results
+    df <- as.data.frame(res) %>%
+      mutate(ensembl_gene_id = rownames(res), .before = 1) %>%
+      left_join(gene.info, by = "ensembl_gene_id") %>%
+      relocate(external_gene_name, .after = ensembl_gene_id) %>%
+      mutate(lfc_shrinkage = lfc_shrinkage) # Add lfc_shrinkage info
+
+    # Add normalized counts (Depth normalized only)
+    norm_counts <- as.data.frame(counts(dds_relevel, normalized = TRUE))
+    norm_counts$ensembl_gene_id <- rownames(norm_counts)
+
+    # Remove columns with count that are not in the current comparison
+    ref_condition <- str_split(clean_name, "_vs_")[[1]][2]
+    alt_condition <- str_split(clean_name, "_vs_")[[1]][1]
+    remove_cols <- dds$sample[
+      !(dds$comb %in% c(ref_condition, alt_condition))
+    ]
+    norm_counts <- norm_counts %>%
+      dplyr::select(
+        -all_of(remove_cols)
+      )
+
+    # Add gene annotation
+    df <- df %>%
+      left_join(norm_counts, by = "ensembl_gene_id") %>%
+      mutate(
+        contrast_name = if (nchar(clean_name) > 31) {
+          as.character(length(resList) + 1)
+        } else {
+          clean_name
+        },
+        class = case_when(
+          grepl("^ENS", ensembl_gene_id, ignore.case = TRUE) ~ genome,
+          TRUE ~ viralgenome
+        )
+      ) %>%
       relocate(class, .after = external_gene_name)
 
-    # Save df to resList
-    resList[[(length(resList) + 1)]] <- df
+    resList[[length(resList) + 1]] <- df
   }
 }
 
@@ -190,11 +237,6 @@ names <- lapply(resList, function(x) unique(x$contrast_name))
 names(resList) <- names
 
 # Write each df to separate csv file
-for (i in seq(resList)){
-  write_csv(resList[[i]],
-            paste0("results/deseq2/", names(resList)[i], ".csv"))
+for (i in seq(resList)) {
+  write_csv(resList[[i]], paste0("results/deseq2/", names(resList)[i], ".csv"))
 }
-
-# Close log file
-sink(log, type = "output")
-sink(log, type = "message")
